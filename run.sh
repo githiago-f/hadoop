1c6832ffb8e1ce9e10582e3f87cb09ed3ba0f448
Thiago <52945005+githiago-f@users.noreply.github.com>
2025-04-25 22:31:29 +0000

    usando docker compose

--- run.sh
+++ run.sh
@@ -1,30 +1,3 @@
-#!/bin/bash
+# instala o hadoop em 1 datanode e 1 namenode
+docker compose up -d
 
-# https://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/
-# https://www.databricks.com/br/glossary/hadoop-distributed-file-system-hdfs
-
-HADOOP_INSTALL_BASE_PATH="/workspaces/hadoop-ins"
-
-if (!(test -d $HADOOP_INSTALL_BASE_PATH)); then
-    mkdir $HADOOP_INSTALL_BASE_PATH
-    curl https://dlcdn.apache.org/hadoop/common/stable/hadoop-3.4.1.tar.gz -L -o $HADOOP_INSTALL_BASE_PATH/hadoop.tar.gz
-    tar -xzvf $HADOOP_INSTALL_BASE_PATH/hadoop.tar.gz -C $HADOOP_INSTALL_BASE_PATH
-    rm -f $HADOOP_INSTALL_BASE_PATH/hadoop.tar.gz
-fi
-
-export PATH="$PATH:$HADOOP_INSTALL_BASE_PATH/hadoop-3.4.1/bin"
-export HADOOP_DIR="$HADOOP_INSTALL_BASE_PATH/hadoop-3.4.1"
-
-if (!(test -d 'input')); then
-    mkdir input
-    cp $HADOOP_DIR/etc/hadoop/*.xml input
-fi
-
-if (test -d 'output'); then
-    rm -rf output
-    mkdir output
-fi
-
-hadoop jar $HADOOP_DIR/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.4.1.jar grep input output 'dfs[a-z.]+'
-
-cat output/*
